b132f6628fa9145f9222136b840c5e3d2de81a42
Jared Jensen <jared.jensen@sage.com>
2015-04-21 17:01:25 -0400

    Unless it

--- lib/chef/provider/azure_sql_server.rb
+++ lib/chef/provider/azure_sql_server.rb
@@ -22,7 +22,7 @@ class Chef
 		   end
 		end
 		
-		if sql_server_exists		
+		unless sql_server_exists
 			properties = csql.create_server("#{new_resource.options[:login]}", \
 											"#{new_resource.options[:password]}", \
 											"#{new_resource.options[:location]}")
