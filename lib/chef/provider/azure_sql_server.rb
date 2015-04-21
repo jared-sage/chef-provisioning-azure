d214eb9c64b27c65cd822d20f227d52ca6535e61
Jared Jensen <jared.jensen@sage.com>
2015-04-21 16:56:31 -0400

    Adding if

--- lib/chef/provider/azure_sql_server.rb
+++ lib/chef/provider/azure_sql_server.rb
@@ -5,6 +5,7 @@ class Chef
     class AzureSqlServer < Chef::Provisioning::AzureDriver::AzureProvider
       action :create do
         restore = Azure.config.management_endpoint
+		sql_server_exists = 0
         Azure.config.management_endpoint = azure_sql_management_endpoint
         Chef::Log.info("Creating AzureSqlServer: #{new_resource.name}")
         csql = Azure::SqlDatabaseManagementService.new
@@ -15,34 +16,37 @@ class Chef
 	       csql.list_sql_server_firewall_rules("#{name}").each do | n |
 		      if n[:rule] == new_resource.name
 			     Azure.config.management_endpoint = restore
-			     fail "SQL server already exists in this subscription."
+			     puts "SQL server already exists in this subscription."
+				 sql_server_exists = 1
 			  end
 		   end
 		end
 		
-        properties = csql.create_server("#{new_resource.options[:login]}", \
-                                        "#{new_resource.options[:password]}", \
-                                        "#{new_resource.options[:location]}")
-        server = properties.name
-		
-		# Create a dummy range for our hack rule
-		range = {
-		   :start_ip_address => "192.168.1.2",
-		   :end_ip_address => "192.168.1.2"
-		}
-		# Add the hack rule
-		csql.set_sql_server_firewall_rule(server, new_resource.name, range)
+		if sql_server_exists		
+			properties = csql.create_server("#{new_resource.options[:login]}", \
+											"#{new_resource.options[:password]}", \
+											"#{new_resource.options[:location]}")
+			server = properties.name
+			
+			# Create a dummy range for our hack rule
+			range = {
+			   :start_ip_address => "192.168.1.2",
+			   :end_ip_address => "192.168.1.2"
+			}
+			# Add the hack rule
+			csql.set_sql_server_firewall_rule(server, new_resource.name, range)
 
-        new_resource.options[:firewall_rules].each do | rule |
-          rule_name = URI::encode(rule[:name])
-          range = {
-            :start_ip_address => rule[:start_ip_address],
-            :end_ip_address => rule[:end_ip_address]
-          }
-          csql.set_sql_server_firewall_rule(server, rule_name, range)
-        end
+			new_resource.options[:firewall_rules].each do | rule |
+			  rule_name = URI::encode(rule[:name])
+			  range = {
+				:start_ip_address => rule[:start_ip_address],
+				:end_ip_address => rule[:end_ip_address]
+			  }
+			  csql.set_sql_server_firewall_rule(server, rule_name, range)
+			end
 
-        Chef::Log.info("Properties of #{new_resource.name}: #{properties.inspect}")
+			Chef::Log.info("Properties of #{new_resource.name}: #{properties.inspect}")
+		end
         Azure.config.management_endpoint = restore
       end
 
