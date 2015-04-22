afb3bacf185fdb26c3b91b84c3c723ea4f682f2b
Jared Jensen <jared.jensen@sage.com>
2015-04-22 17:53:31 -0400

    Azue SB queue files

---
+++ lib/chef/provider/azure_queue.rb
@@ -0,0 +1,31 @@
+require 'chef/provisioning/azure_driver/azure_provider'
+
+class Chef
+  class Provider
+    class AzureQueue < Chef::Provisioning::AzureDriver::AzureProvider
+      action :create do
+		restore_namespace = Azure.config.sb_namespace
+		restore_key = Azure.config.sb_access_key
+		Azure.config.sb_namespace = new_resource.options.namespace
+		Azure.config.sb_access_key =  new_resource.options.access_key		
+        Chef::Log.info("Creating AzureQueue: #{new_resource.name} in AzureServiceBus ${new_resource.options.namespace}")		
+        sbs = Azure::ServiceBusService.new
+        sbs.create_queue(new_resource.name)		
+		Azure.config.sb_namespace = restore_namespace
+		Azure.config.sb_access_key = restore_key
+      end
+
+      action :destroy do
+		restore_namespace = Azure.config.sb_namespace
+		restore_key = Azure.config.sb_access_key
+		Azure.config.sb_namespace = new_resource.options.namespace
+		Azure.config.sb_access_key =  new_resource.options.access_key
+        Chef::Log.info("Destroying AzureQueue: #{new_resource.name} in AzureServiceBus ${new_resource.options.namespace}")
+        sbs = Azure::ServiceBusService.new
+        sbs.delete_queue(new_resource.name)
+		Azure.config.sb_namespace = restore_namespace
+		Azure.config.sb_access_key = restore_key
+      end
+    end
+  end
+end
