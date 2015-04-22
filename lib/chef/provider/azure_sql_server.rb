8f08f71694f71220ba3665f5803925045d8c264e
Jared Jensen <jared.jensen@sage.com>
2015-04-22 10:40:40 -0400

    Fixing delete

--- lib/chef/provider/azure_sql_server.rb
+++ lib/chef/provider/azure_sql_server.rb
@@ -15,45 +15,66 @@ class Chef
            name = m.name
 	       csql.list_sql_server_firewall_rules("#{name}").each do | n |
 		      if n[:rule] == new_resource.name
-			     Azure.config.management_endpoint = restore
 			     puts "SQL server already exists in this subscription."
-				 return
+				 sql_server_exists = 1
+				 break
 			  end
 		   end
+		   if sql_server_exists == 1
+			 break
+		  end
 		end
-
-		properties = csql.create_server("#{new_resource.options[:login]}", \
-										"#{new_resource.options[:password]}", \
-										"#{new_resource.options[:location]}")
-		server = properties.name
 		
-		# Create a dummy range for our hack rule
-		range = {
-		   :start_ip_address => "192.168.1.2",
-		   :end_ip_address => "192.168.1.2"
-		}
-		# Add the hack rule
-		csql.set_sql_server_firewall_rule(server, new_resource.name, range)
+		if sql-server_exists == 0 
+			properties = csql.create_server("#{new_resource.options[:login]}", \
+											"#{new_resource.options[:password]}", \
+											"#{new_resource.options[:location]}")
+			server = properties.name
+			
+			# Create a dummy range for our hack rule
+			range = {
+			   :start_ip_address => "192.168.1.2",
+			   :end_ip_address => "192.168.1.2"
+			}
+			# Add the hack rule
+			csql.set_sql_server_firewall_rule(server, new_resource.name, range)
 
-		new_resource.options[:firewall_rules].each do | rule |
-		  rule_name = URI::encode(rule[:name])
-		  range = {
-			:start_ip_address => rule[:start_ip_address],
-			:end_ip_address => rule[:end_ip_address]
-		  }
-		  csql.set_sql_server_firewall_rule(server, rule_name, range)
+			new_resource.options[:firewall_rules].each do | rule |
+			  rule_name = URI::encode(rule[:name])
+			  range = {
+				:start_ip_address => rule[:start_ip_address],
+				:end_ip_address => rule[:end_ip_address]
+			  }
+			  csql.set_sql_server_firewall_rule(server, rule_name, range)
+			end
+
+			Chef::Log.info("Properties of #{new_resource.name}: #{properties.inspect}")
 		end
-
-		Chef::Log.info("Properties of #{new_resource.name}: #{properties.inspect}")
-        Azure.config.management_endpoint = restore
+		Azure.config.management_endpoint = restore
       end
 
       action :destroy do
         restore = Azure.config.management_endpoint
+		sql_server_exists = 0
         Azure.config.management_endpoint = azure_sql_management_endpoint
         Chef::Log.info("Destroying AzureSQLServer: #{new_resource.name}")
         csql = Azure::SqlDatabaseManagementService.new
-		csql.delete_server(new_resource.name)
+		# hack!
+		csql.list_servers.each do | m |
+           name = m.name
+	       csql.list_sql_server_firewall_rules("#{name}").each do | n |
+		      if n[:rule] == new_resource.name
+				 sql_server_exists = name
+				 break
+			  end
+		   end
+		   if sql_server_exists != 0 
+		      break
+		   end
+		end
+		if sql_server_exists != 0
+		   csql.delete_server(sql_server_exists)
+		end
 		Azure.config.management_endpoint = restore
       end
     end
